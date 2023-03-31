2a3fde56723e82e3fbc53292023b14ed02486844
yashbujji <36237332+yashbujji@users.noreply.github.com>
2023-03-30 23:15:37 -0700

    lc217

---
+++ python/lc217.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # if len(nums) <= 1:
+        #     return True 
+        nums.sort()
+        left = 0
+        right =  left + 1
+        while right < len(nums):
+            if nums[left] == nums[right]:
+                return True
+            left = left +1
+            right = right +1
+        return False
